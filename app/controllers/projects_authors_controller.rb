6e5a651dae842c490b144f635e12a2138574d751
Valentin Apostolov <valentin.apostolov@gmail.com>
2020-02-16 14:04:09 +0200

    Add an author to a project

--- app/controllers/projects_authors_controller.rb
+++ app/controllers/projects_authors_controller.rb
@@ -2,13 +2,25 @@ class ProjectsAuthorsController < ApplicationController
   def index
     @users = User.all
     @project_id = params[:project_id]
+    @current_author = ProjectsAuthors.find_by(    project_id: @project_id)
+    @current_author_id = @current_author.nil? ? nil : @current_author.author_id
   end
 
   def save
-    projects_authors = ProjectsAuthors.create(    project_id: params[:project_id],     author_id: params[:user_id])
+    if params[:user_id].blank?
+      projects_authors = ProjectsAuthors.find_by(      project_id: params[:project_id])
 
-    if projects_authors.save
-      flash[:notice] = params[:user_id]
+      unless projects_authors.nil?
+        projects_authors.destroy
+        flash[:notice] = "Author has been deleted"
+      end
+
+    else
+      projects_authors = ProjectsAuthors.create(      project_id: params[:project_id],       author_id: params[:user_id])
+
+      if projects_authors.save
+        flash[:notice] = "Author has been set"
+      end
     end
 
     redirect_to(    :action => "index")

20695a46cac5c890966acef015d2d474f826343a
Valentin Apostolov <valentin.apostolov@gmail.com>
2020-02-17 15:56:17 +0200

    clean up

--- init.rb
+++ init.rb
@@ -5,7 +5,6 @@ Redmine::Plugin.register :project_author do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
-  require_dependency 'project_author_hook_listener'
   require_dependency 'override_render_project_hierarchy'
   permission :projects_authors, { :projects_authors => [:index, :save] }, :public => true
   menu :project_menu, :projects_authors, { :controller => 'projects_authors', :action => 'index' }, :caption => 'Author', :after => :activity, :param => :project_id

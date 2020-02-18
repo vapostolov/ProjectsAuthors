6c6cc88fc0c56d7c3eb617526a5bc6a465dfb620
Valentin Apostolov <valentin.apostolov@gmail.com>
2020-02-18 10:23:09 +0200

    Simple functional test

--- test/functional/projects_authors_controller_test.rb
+++ test/functional/projects_authors_controller_test.rb
@@ -1,8 +1,16 @@
-require File.expand_path('../../test_helper', __FILE__)
+require File.expand_path("../../test_helper", __FILE__)
 
 class ProjectsAuthorsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :projects
+  plugin_fixtures :projects_authors, :enabled_modules
+
+  def setup
+    @project = Project.find(1)
+  end
+
+  def test_index
+    get(:authors,     :project_id => @project.identifier)
+    assert_response(:success)
+    assert_template("index")
   end
 end
